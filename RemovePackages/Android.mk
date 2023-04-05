b1e77cc6929b0aedd7f45593e787bed2b56cc62b
Emil <emilwagenplast@gmail.com>
2023-04-05 09:18:28 +0200

    raphael: remove more packages at build time

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,11 +4,12 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Aperture AmbientSensePrebuilt YouTube Maps Drive
+LOCAL_OVERRIDES_PACKAGES := Aperture AmbientSensePrebuilt YouTube Maps Drive Chrome-Stub PrebuiltGmail CalendarGooglePrebuilt
 LOCAL_OVERRIDES_PACKAGES += DevicePolicyPrebuilt
 LOCAL_OVERRIDES_PACKAGES += Photos
 LOCAL_OVERRIDES_PACKAGES += PixelLiveWallpaperPrebuilt
 LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt
+LOCAL_OVERRIDES_PACKAGES += yetCalc Phonograph
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
